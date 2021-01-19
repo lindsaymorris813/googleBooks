3aa134d21e9c4b162a70cb50cbd1ee17d4b254d4
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-19 15:53:53 -0500

    jumbotron, navbar

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Jumbotron({ children }) {
+  return (
+    <div className="jumbotron align-center">
+      <h1 className="display-4">(React) Google Books Search</h1>
+      <p class="lead">Search for and Save Books</p>
+    </div>
+  );
+}
+
+export default Jumbotron;
+
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <a className="navbar-brand" href="/">
+        Google Books
+      </a>
+    </nav>
+  );
+}
+
+export default NavBar;
