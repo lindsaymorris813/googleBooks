0f8fe2a4bfb49ebfa61a0ac53242db092873c120
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-19 15:59:02 -0500

    Router, Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import JumboTron from "./components/Jumbotron";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <NavBar />
       <JumboTron />
+      <Switch>
+        <Route exact path="/" component={ Search } />
+        <Route exact path="/saved" component= { Saved } />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
